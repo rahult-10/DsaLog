e7c7843627ea7bd6755370588714fb3c46ad719a
rahult-10 <rahult0710@gmail.com>
2026-04-12 12:38:01 +0530

    updated code

--- string/lcs.java
+++ string/lcs.java
@@ -1,6 +1,6 @@
 // Longest Common SubSequence 
 
-public static int lcs(String text1, String text2) {
+/* public static int lcs(String text1, String text2) {
     return helper(text1, text2, text1.length(), text2.length());
 }
 
@@ -16,6 +16,26 @@ private static int helper(String text1, String text2, int m, int n) {
         helper(text1, text2, m - 1, n),
         helper(text1, text2, m, n - 1)
     );
+} */
+
+//M2 -> DP
+public static int lcs(String text1, String text2) {
+    int m = text1.length();
+    int n = text2.length();
+
+    int[][] dp = new int[m + 1][n + 1];
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[m][n];
 }
 
 public static void main(String[] args) {
