e3bcc33a4ac589030cf771405e405c1245bf9edf
rahult-10 <rahult0710@gmail.com>
2026-04-14 22:39:28 +0530

    Using Memoization

--- DP/climbStairs.java
+++ DP/climbStairs.java
@@ -1,10 +1,27 @@
 //Using Recursion
-public int climbStairs(int n) {
-    if(n == 0)
+// public int climbStairs(int n) {
+//     if(n == 0)
+//         return 1;
+//     if(n < 0)
+//         return 0;
+//     return climbStairs(n-1) + climbStairs(n-2);
+// }
+
+// Using DP -> Memoization
+public int memoization(int n, int[] dp){
+    if (n == 0)
         return 1;
-    if(n < 0)
+    if (n < 0)
         return 0;
-    return climbStairs(n-1) + climbStairs(n-2);
+    if (dp[n] != -1) {        return dp[n];
+    }
+    dp[n] = memoization(n-1,dp) + memoization(n - 2, dp);
+    return dp[n];
+}
+public int climbStairs(int n){
+    int[] dp = new int[n+1];
+    Arrays.fill(dp, -1);
+    return memoization(n, dp);
 }
 
 public void main(String[] args) {
