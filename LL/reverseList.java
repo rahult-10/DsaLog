8f36f2bd0a2578964ea414d3806b6e1d991ac606
rahult-10 <rahult0710@gmail.com>
2026-04-17 22:36:21 +0530

    Using Stack

---
+++ LL/reverseList.java
@@ -0,0 +1,27 @@
+//Reverse a singly linked list.
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public ListNode reverseList(ListNode head) {
+    ListNode temp = head;
+    Stack<Integer> st = new Stack<>();
+    while(temp != null){
+        st.push(temp.val);
+        temp = temp.next;
+    }
+    temp = head;
+    while(temp != null){
+        temp.val = st.peek();
+        st.pop();
+        temp = temp.next;
+    }
+    return head;
+}
+
+public static void main(String[] args) {
+    return;
+}
