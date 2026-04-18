a05692b8d0dfc90fabb2fd7dba14f2fa10ec7cf0
rahult-10 <rahult0710@gmail.com>
2026-04-18 12:23:10 +0530

    Remove Nth Node

---
+++ LL/removeNthNode.java
@@ -0,0 +1,39 @@
+// 19. Remove Nth Node From End of List
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+ 
+
+public ListNode removeNthNode(ListNode head, int n) {
+    int cnt = 0;
+    ListNode temp = head;
+    while(temp != null){
+        cnt++;
+        temp = temp.next;
+    }
+    if(cnt == n){
+        ListNode newHead = head.next;
+        return newHead;
+    }
+    int result = cnt - n;
+    temp = head;
+    while(temp != null){
+        result--;
+        if(result == 0){
+            break;
+        }
+        temp = temp.next;
+    }
+    ListNode remove = temp.next;
+    temp.next = temp.next.next;
+    return head;
+}
+
+public static void main(String[] args) {
+    return;
+}
---
+++ recursion/knapSack.java
@@ -0,0 +1,24 @@
+//0-1 Knapsack 
+
+public static int knapSack(int[] val, int[] wt, int W, int n){
+    if(W == 0 || n ==0){
+        return 0;
+    }
+
+    if(wt[n-1] <= W){
+        int ans1 = val[n-1] + knapSack(val, wt, W-wt[n-1], n-1);
+        int ans2 = knapSack(val, wt, W, n-1);
+        return Math.max(ans1, ans2);
+    }
+    else {
+        return knapSack(val, wt, W, n-1);
+    }
+}
+
+public static void main(String[] args){
+    int[] val = {15, 14, 10, 45, 30};
+    int[] wt = {2, 5, 1, 3, 4};
+    int W = 7;
+
+    System.out.println(knapSack(val, wt, W, val.length));
+}
