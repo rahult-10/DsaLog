d63262c05bbe0498377d8ad85768ffd443c2bad6
rahult-10 <rahult0710@gmail.com>
2026-04-19 15:06:24 +0530

    Using fast and slow pointer

--- LL/linkedListCycle.java
+++ LL/linkedListCycle.java
@@ -22,6 +22,19 @@ public boolean hasCycle(ListNode head) {
     return false;
 }
 
+public boolean cycle(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(slow == fast)
+            return true;
+    }
+    return false;
+}
+
 public static void main(String[] args) {
     
 }
