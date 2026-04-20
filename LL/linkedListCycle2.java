ff4258ab83d83cb895898a6993e1704a89905657
rahult-10 <rahult0710@gmail.com>
2026-04-20 11:41:57 +0530

    Using Slow & Fast

--- LL/linkedListCycle2.java
+++ LL/linkedListCycle2.java
@@ -23,6 +23,25 @@ public ListNode detect(ListNode head) {
     return null;
 }
 
+//M2 -> slow & fast
+public ListNode detectCycle(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow == fast){
+            slow = head;
+            while(slow != fast){
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+    }
+    return null;
+}
+
 public static void main(String[] args) {
     
 }
