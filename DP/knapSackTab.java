47d5dd697ce1f462dc3e6d10563df1c2f4bd1d96
rahult-10 <rahult0710@gmail.com>
2026-04-20 23:00:45 +0530

    Using Tabulation

---
+++ DP/knapSackTab.java
@@ -0,0 +1,41 @@
+//0-1 Knapsack 
+
+//Using Tabulation
+public static int knapSackTab(int[] val, int[] wt, int W){
+    int n = val.length;
+    int dp[][] = new int[n+1][W+1];
+
+    for(int i = 0; i < dp.length; i++){
+        dp[i][0] = 0;
+    }
+
+    for(int j = 0; j < dp[0].length; j++){
+        dp[0][j] = 0;
+    }
+
+    for(int i = 1; i < n+1; i++){
+        for(int j = 1; j < W+1; j++) {
+            int v = val[i-1];
+            int w = wt[i-1];
+
+            if(w <= j){
+                int includeProfit = v + dp[i-1][j-w];
+                int excludeProfit = dp[i-1][j];
+                dp[i][j] = Math.max(includeProfit, excludeProfit);
+            }
+            else{
+                int excludeProfit = dp[i-1][j];
+                dp[i][j] = excludeProfit;
+            }
+        }
+    }
+    return dp[n][W];
+}
+
+public static void main(String[] args){
+    int[] val = {15, 14, 10, 45, 30};
+    int[] wt = {2, 5, 1, 3, 4};
+    int W = 7;
+    
+    System.out.println(knapSackTab(val, wt, W));
+}
