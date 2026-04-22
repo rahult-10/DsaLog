cb14b504db597bd20c4fc8daa2bbfa55ad57a39c
rahult-10 <rahult0710@gmail.com>
2026-04-22 14:42:21 +0530

    Search in Rotated Sorted Array

---
+++ BinarySearch/searchInRotateArray.java
@@ -0,0 +1,36 @@
+//33. Search in Rotated Sorted Array
+
+      
+public int searchInRotateArray(int[] nums, int target) {
+    int n = nums.length;
+    int low = 0;
+    int high = n - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target <= nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        } else {
+            if (nums[mid] <= target && target <= nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+public void main(String[] args) {
+    int[] nums = {4,5,6,7,0,1,2};
+    int target = 0;
+    int result = searchInRotateArray(nums,target);
+    System.out.println(result);
+}
