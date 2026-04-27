dfe4e3286509558244ad784e1584c1f81bd48f60
rahult-10 <rahult0710@gmail.com>
2026-04-28 00:26:02 +0530

    Search In 2D matrix -> optimized (Binary Search

---
+++ BinarySearch/searchInMatrix.java
@@ -0,0 +1,33 @@
+//Search In 2D Matrix
+
+public boolean searchInMatrix(int[][] matrix, int target) {
+    int m = matrix.length;
+    int n = matrix[0].length;
+    int low = 0;
+    int high = (m*n-1);
+    while(low <= high){
+        int mid = low + (high-low)/2;
+
+        int row = mid / n;
+        int col = mid % n;
+
+        if(matrix[row][col] == target){
+            return true;
+        }
+        else if(matrix[row][col] < target){
+            low = mid+1;
+        }
+        else {
+            high = mid-1;
+        }
+
+    }
+    return false;
+}
+
+public void main(String[] args) {
+    int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+    int target = 3;
+    boolean result = searchInMatrix(matrix, target);
+    System.out.println(result);
+}
