ad4ac197b623eb1e6a0ab959ce0bfc3531da23b1
rahult-10 <rahult0710@gmail.com>
2026-04-27 23:45:50 +0530

    Optimized Using Binary Search

--- BinarySearch/peakEle2.java
+++ BinarySearch/peakEle2.java
@@ -18,6 +18,46 @@ public int[] peakEle2(int[][] mat) {
     return new int[]{-1, -1};
 }
 
+//M2 -> Using Binary Search
+public int[] findPeakGrid(int[][] mat) {
+    int m = mat.length;
+    int n = mat[0].length;
+    int low = 0;
+    int high = n-1;
+    while(low <= high){
+
+        int mid = low+(high-low)/2;
+        int row = maxElement(mat, mid);
+            
+        int left = (mid-1 >= 0)? mat[row][mid-1] : -1;
+        int right = (mid+1 < n)? mat[row][mid+1] : -1;
+
+        if(mat[row][mid] > left && mat[row][mid] > right){
+            return new int[]{row, mid};
+        }
+        else if(mat[row][mid] < left){
+            high = mid-1;
+        }
+        else {
+            low = mid+1;
+        }
+    }
+    return new int[]{-1,-1};
+}
+
+public static int maxElement(int[][] mat, int col){
+    int n = mat.length;
+    int maxValue = -1;
+    int index = -1;
+    for(int i = 0; i < n; i++){
+        if(mat[i][col] > maxValue){
+            maxValue = mat[i][col];
+            index = i;
+        }
+    }
+    return index;
+}
+
 public void main(String[] args) {
     int[][] mat = {{10,20,15}, {21,30,14}, {7,16,32}};
     int[] result = peakEle2(mat);
