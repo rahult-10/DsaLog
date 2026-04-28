3a1259c1779e0733fbc283e24542e458ca7380bf
rahult-10 <rahult0710@gmail.com>
2026-04-28 15:26:06 +0530

    Perfect Squares -> Tabulation

--- DP/perfectSquares.java
+++ DP/perfectSquares.java
@@ -19,6 +19,22 @@ private int solve(int n, int[] memo) {
     return min;
 }
 
+
+// Using Tabulation
+public int numSquares(int n) {
+    int[] dp = new int[n + 1];
+    for (int i = 1; i <= n; i++) {
+        dp[i] = Integer.MAX_VALUE;
+
+        for (int j = 1; j * j <= i; j++) {
+            dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+        }
+    }
+    return dp[n];
+}
+
+
+
 public void main(String[] args) {
     int n = 56;
     int result = perfectSquares(n);
