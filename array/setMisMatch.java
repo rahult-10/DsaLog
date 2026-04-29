e60b9edd1e60256994f13f304a39ca7351575c10
rahult-10 <rahult0710@gmail.com>
2026-04-29 22:18:47 +0530

    Set MisMatch

--- array/setMisMatch.java
+++ array/setMisMatch.java
@@ -21,6 +21,28 @@ public int[] setMismatch(int[] nums) {
     return new int[]{dup, missing};
 }
 
+public int[] findErrorNums(int[] nums) {
+    int dup = -1;
+    int n = nums.length;
+
+    for(int i = 0; i < n; i++){
+        int idx = Math.abs(nums[i])-1;
+
+        if(nums[idx] <= 0){
+            dup = Math.abs(nums[i]);
+        } else {
+            nums[idx] *= -1;
+        }
+    }
+
+    for(int i = 0; i< n; i++){
+        if(nums[i] > 0){
+            return new int[] {dup, i+1};
+        }
+    }
+    return new int[] {-1,-1};
+}
+
 public void main(String[] args) {
     int[] nums = {1,2,2,4};
     int[] result = setMismatch(nums);
