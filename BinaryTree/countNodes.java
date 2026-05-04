7a17ca10a192ddffce8cd11d975690510111e1ad
rahult-10 <rahult0710@gmail.com>
2026-05-04 15:56:19 +0530

    Count Complete Tree Nodes

---
+++ BinaryTree/countNodes.java
@@ -0,0 +1,25 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public int countNodes(TreeNode root) {
+    if(root == null){
+        return 0;
+    }
+    int leftNodes = countNodes(root.left);
+    int rightNodes = countNodes(root.right);
+    return leftNodes+rightNodes+1;
+}
+
+public void main(String[] args){
+
+}
