cdbddfd4a49429bfdfb1e8e7faeb97d91e6739f1
rahult-10 <rahult0710@gmail.com>
2026-05-06 18:52:04 +0530

    InOrder Traversal

---
+++ BinaryTree/inorderTraversal.java
@@ -0,0 +1,32 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    inorder(root, result);
+    return result;
+}
+
+private void inorder(TreeNode root, List<Integer>result){
+    if(root == null){
+        return;
+    }
+    inorder(root.left, result);
+    result.add(root.val);
+    inorder(root.right, result);
+}
+
+public static void main(String[] args) {
+    
+}
