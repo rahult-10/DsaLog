34f33db7bb300d56571314ee4827cbd29f563103
rahult-10 <rahult0710@gmail.com>
2026-05-07 22:14:58 +0530

    Stone Game

---
+++ games/stoneGame.java
@@ -0,0 +1,9 @@
+public boolean stoneGame(int[] piles) {
+    return true;
+}
+
+public void main(String[] args) {
+    int[] piles = {5,4,3,5};
+    boolean result = stoneGame(piles);
+    System.out.println(result);
+}
