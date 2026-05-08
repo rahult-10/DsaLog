ae399d0308ae7497ec6eb15d9dd0e11d15f15ba2
rahult-10 <rahult0710@gmail.com>
2026-05-08 15:26:53 +0530

    Next Greater Element 1

--- Stack/nextGreaterElement.java
+++ Stack/nextGreaterElement.java
@@ -19,9 +19,33 @@ public int[] nextGreaterElement(int[] nums1, int[] nums2) {
     return nge;
 }
 
+//Optimized -> Using Stack, HashMap
+public int[] ngElement(int[] nums1, int[] nums2){
+    Map<Integer, Integer> map = new HashMap<>();
+    Deque<Integer> stack = new ArrayDeque<>();
+
+    for(int num : nums2){
+        while(!stack.isEmpty() && num > stack.peek()){
+            map.put(stack.pop(), num);
+        }
+        stack.push(num);
+    }
+    while(!stack.isEmpty()){
+        map.put(stack.pop(), -1);
+    }
+    int[] result = new int[nums1.length];
+    for(int i = 0; i < nums1.length; i++){
+        result[i] = map.get(nums1[i]);
+    }
+    return result;
+}
+
+
 public void main(String[] args) {
     int nums1[] = {4,1,2};
     int nums2[] = {1,3,4,2};
     int[] res = nextGreaterElement(nums1, nums2);
     System.out.println(Arrays.toString(res));
+    int[] ans = ngElement(nums1, nums2);
+    System.out.println(Arrays.toString(ans));
 }
