0a38db7c942f05b71cdea7516257a49bb80dcc37
rahult-10 <rahult0710@gmail.com>
2026-05-09 11:58:29 +0530

    Next Greater Element 2

--- Stack/nextGreaterElements.java
+++ Stack/nextGreaterElements.java
@@ -14,8 +14,35 @@ public int[] nextGreaterElements(int[] nums) {
     return nge;
 }
 
+
+public int[] ngElement(int[] nums){
+    int n = nums.length;
+    int[] nge = new int[n];
+    Arrays.fill(nge, -1);
+
+    Stack<Integer> st = new Stack<>();
+    for (int i = 2 * n - 1; i >= 0; i--) {
+        int idx = i % n;
+
+        while (!st.isEmpty() && nums[st.peek()] <= nums[idx]) {
+            st.pop();
+        }
+
+        if (i < n) {
+            if (!st.isEmpty()) {
+                nge[idx] = nums[st.peek()];
+            }
+        }
+
+        st.push(idx);
+    }
+
+    return nge;
+}
+
 public void main(String[] args) {
     int[] nums= {1,2,3,4,3};
     int[] res = nextGreaterElements(nums);
-    System.out.println(Arrays.toString(res));
+    int[] result = ngElement(nums);
+    System.out.println(Arrays.toString(res) + "  " + Arrays.toString(result));
 }
