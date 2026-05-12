5b1bf37473031a97e2efe60e05bd71c53aeaa0a7
rahult-10 <rahult0710@gmail.com>
2026-05-12 23:26:06 +0530

    Contains Duplicate

---
+++ array/containsDuplicate.java
@@ -0,0 +1,16 @@
+public boolean containsDuplicate(int[] nums) {
+    HashSet<Integer> set = new HashSet<>();
+    for(int num : nums){
+        if(set.contains(num)){
+            return true;
+        }
+        set.add(num);
+    }
+    return false;
+}
+
+public void main(String[] args){
+    int[] nums = {1,2,3,1};
+    boolean result = containsDuplicate(nums);
+    System.out.println(result);
+}
