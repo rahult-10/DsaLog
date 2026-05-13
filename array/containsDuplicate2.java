19f4121a60ad96dae0cb23f2d2d2584f295f78cb
rahult-10 <rahult0710@gmail.com>
2026-05-13 22:53:29 +0530

    Contains Duplicate 2

--- array/containsDuplicate2.java
+++ array/containsDuplicate2.java
@@ -9,9 +9,26 @@ public boolean containsDuplicate2(int[] nums, int k) {
     return false;
 }
 
+public boolean containsNearbyDuplicate(int[] nums, int k) {
+    int n = nums.length;
+    HashSet<Integer> set = new HashSet<>();
+    for(int i = 0; i < n; i++){
+        if(i > k){
+            set.remove(nums[i-k-1]);
+        }
+
+        if(set.contains(nums[i])){
+            return true;
+        }
+        set.add(nums[i]);
+    }
+    return false;
+}
+
 public void main(String[] args){
     int[] nums = {1,2,3,1};
     int k = 3;
     boolean result = containsDuplicate2(nums,k);
-    System.out.println(result);
+    boolean res = containsNearbyDuplicate(nums, k);
+    System.out.println(result + "," + res);
 }
