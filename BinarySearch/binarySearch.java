8f41d1aff157ce02d234f6fb903cef0049f7c9a7
rahult-10 <rahult0710@gmail.com>
2026-05-14 23:02:39 +0530

    Binary Search

--- BinarySearch/binarySearch.java
+++ BinarySearch/binarySearch.java
@@ -8,9 +8,28 @@ public int search(int[] nums, int target) {
     return -1;
 }
 
+public int binarySearch(int[] nums, int target){
+    int n = nums.length;
+    int left = 0;
+    int right = n-1;
+    while(left <= right){
+        int mid = left + (right - left)/2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(nums[mid] < target){
+            left = mid+1;
+        }
+        else {
+            right = mid-1;
+        }
+    }
+    return -1;
+}
+
 public void main(String[] args){
     int[] nums = {-1,0,3,5,9,12};
     int target = 9;
-    int result = search(nums, target);
+    int result = binarySearch(nums, target);
     System.out.println(result);
 }
